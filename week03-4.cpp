738cc4ffadf79cc3c2a7ce943d58d2a346c868a8
Yen81693 <114201603+Yen81693@users.noreply.github.com>
2022-09-26 10:14:46 +0800

    Create week03-4.cpp

---
+++ week03-4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    printf("½Ð¿é¤J1­Ó¼Æ¦r:");
+    int n;
+    scanf("%d",&n);
+    printf("¥Î8°£¾l%d\n",n%8);
+    printf("¥Î9°£¾l%d\n",n%9);
+    printf("¥Î15°£¾l%d\n",n%15);
+}
